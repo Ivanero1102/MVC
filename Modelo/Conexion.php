f50ce0dd85e5038d8a6ebf5e59e34c57b0723900
ivanero1102 <ivanero1102@gmail.com>
2022-12-15 21:37:15 +0100

    Avances

--- Modelo/Conexion.php
+++ Modelo/Conexion.php
@@ -1,19 +1,18 @@
-<?php
-class Conexion{
-    
-    protected function realizaConexion(){
-        $server = "localhost";
-        $user = "root";
-        $clave= "";
-        $BD="protectora_animales";
-        try {
-        $conn = new PDO("mysql:host=".$this->server.";dbname=".$this->BD.", ".$this->user.", ". $this->clave);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexion exitosa";
-        return $conn;
-        }catch(PDOException $e){
-            echo "Conexion fallida". $e->getMessage();
+<?php  
+    class Conexion{
+        private $server = "localhost";
+        private $user = "root";
+        private $clave= "";
+        private $BD="protectora_animales";
+        public function realizaConexion(){
+            try {
+            $conn = new PDO("mysql:host=".$this->server.";dbname=".$this->BD.";",$this->user,  $this->clave);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Conexion exitosa";
+            return $conn;
+            }catch(PDOException $e){
+                echo "Conexion fallida". $e->getMessage();
+            }
         }
     }
-}
 ?>
--- Modelo/Crud.php
+++ Modelo/Crud.php
@@ -1,31 +1,33 @@
 <?php
-abstract class Crud extends Conexion{
-private $tabla;
-private $conexion;
-
-function __construct($tabla){
-    $this->tabla = $tabla;
-    $this->conexion = parent::realizaConexion();
-}
-function __get($variable){
-    return $this->$variable;
-}
-function __set($variable, $almacen){
-    $this->$variable = $almacen;
-}
-function obtieneTodos(){
+    abstract class Crud extends Conexion{
+        private $tabla;
+        private $conexion;
     
-}
-
-function obtieneDeID(){
+        function __construct($tabla){
+            $this->tabla = $tabla;
+            $this->conexion = parent::realizaConexion();
+        }
     
-}
-
-function borrar(){
-
-}
-
-abstract function crear();
-abstract function actualizar();
-}
+        function obtieneTodos(){
+            $sql = "SELECT * FROM ". $this->tabla.";";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+    
+        function obtieneDeID($id){
+            $sql = "SELECT * FROM $this->tabla WHERE id = ? ;";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindParam(1,$id);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_OBJ);
+        }
+    
+        // function borrar(){
+    
+        // }
+    
+        // abstract function crear();
+        // abstract function actualizar();
+    }
 ?>
